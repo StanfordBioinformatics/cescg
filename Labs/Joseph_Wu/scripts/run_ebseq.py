25e2eb75f1e70746c93b4d0aa328411246439329
Nathaniel Watson <nathankw@stanford.edu>
2016-07-25 22:54:16 -0700

    updated workflow id

--- Labs/Joseph_Wu/scripts/run_ebseq.py
+++ Labs/Joseph_Wu/scripts/run_ebseq.py
@@ -4,6 +4,52 @@ import dxpy
 class NoControlBarcode(Exception):
 	pass
 
+
+def run_gene_ebseq(treatment_folder,control_folder,patient_id):
+	"""
+	Runs the 'rsem-ebseq gene expression' workflow I wrote.
+	"""
+	gene_glob = "*.genes.results"
+	quant_file = dxpy.find_one_data_object(more_ok=False,project=jo_wu_project,folder=treatment_folder,name=gene_glob,name_mode="glob")
+	quant_file = dxpy.dxlink(project_id=quant_file['project'],object_id=quant_file['id'])
+	control_quant_file = dxpy.find_one_data_object(more_ok=False,project=jo_wu_project,folder=control_folder,name=gene_glob,name_mode="glob")
+	control_quant_file = dxpy.dxlink(project_id=control_quant_file['project'],object_id=control_quant_file['id'])
+	#the quant_files are the isoforms gene expression files created by rsem-calculate-expression
+	treatment_barcode = os.path.split(treatment_folder)[-1]
+	control_barcode = os.path.split(control_folder)[-1]
+	wf = dxpy.DXWorkflow(project="project-BxxYqbQ0v3VQz5z2bvFyF6YV",dxid="workflow-ByFG8g00v3VpjkVxP0gkpX7X")
+	destination_folder = os.path.join(treatment_folder,"ebseq")
+	workflow_input = {"0.results":[quant_file,control_quant_file],"1.conditions": "1,1"}
+	job_properties = {"patient_id":patient_id,"control_barcode":control_barcode,"treatment_barcode":treatment_barcode}
+	job_name = "_".join([patient_id,treatment_barcode,control_barcode,wf.name])
+	wf.run(debug={"debugOn":["AppError","AppInternalError"]},workflow_input=workflow_input,project=jo_wu_project,folder=destination_folder,name=job_name,properties=job_properties)
+	
+
+def run_isoform_ebseq(treatment_folder,control_folder,patient_id):
+	"""
+	Runs the 'rsem-ebseq isoform expression' workflow I wrote.
+	"""
+	isoform_glob = "*.isoforms.results"
+	quant_file = dxpy.find_one_data_object(more_ok=False,project=jo_wu_project,folder=treatment_folder,name=isoform_glob,name_mode="glob")
+	quant_file = dxpy.dxlink(project_id=quant_file['project'],object_id=quant_file['id'])
+	control_quant_file = dxpy.find_one_data_object(more_ok=False,project=jo_wu_project,folder=control_folder,name=isoform_glob,name_mode="glob")
+	control_quant_file = dxpy.dxlink(project_id=control_quant_file['project'],object_id=control_quant_file['id'])
+	#the quant_files are the isoforms gene expression files created by rsem-calculate-expression
+	treatment_barcode = os.path.split(treatment_folder)[-1]
+	control_barcode = os.path.split(control_folder)[-1]
+	wf = dxpy.DXWorkflow(project="project-BxxYqbQ0v3VQz5z2bvFyF6YV",dxid="workflow-ByFPY900v3Vbb8XJVxvzJ1f7")
+	destination_folder = os.path.join(treatment_folder,"ebseq")
+	transcripts_file = dxpy.dxlink(project_id="project-BxZpbXj0V610b5Q6x1FV80gb",object_id="file-ByBpjPQ0V61585gqXkvQ11Z5")
+	workflow_input = {"0.results":[quant_file,control_quant_file],"1.input_fasta_file":transcripts_file,"1.output_name":"isoforms.results","2.conditions": "1,1"}
+	job_properties = {"patient_id":patient_id,"control_barcode":control_barcode,"treatment_barcode":treatment_barcode}
+	job_name = "_".join([patient_id,treatment_barcode,control_barcode,wf.name])
+	wf.run(debug={"debugOn":["AppError","AppInternalError"]},workflow_input=workflow_input,project=jo_wu_project,folder=destination_folder,name=job_name,properties=job_properties)
+	
+	
+    #ebseq_folder = os.path.join(barcode_folder,"ebseq")
+    #p.new_folder(ebseq_folder)
+				
+
 jo_wu_project = "project-BxZpbXj0V610b5Q6x1FV80gb"
 
 control_barcodes = ["ATTACTCG-AGGCTATA","ATTACTCG-TAAGATTA","TCCGGAGA-AGGATAGG","TCCGGAGA-TAAGATTA","CGCTCATT-AGGCTATA"]
@@ -32,7 +78,7 @@ p = dxpy.DXProject(jo_wu_project)
 patient_folders = p.list_folder(only="folders",folder="/encode_long_rnaseq")["folders"]
 for pf in patient_folders:
 	patient_id = os.path.split(pf)[-1]
-  barcode_folders = p.list_folder(folder=pf,only="folders")["folders"]
+	barcode_folders = p.list_folder(folder=pf,only="folders")["folders"]
 	if len(barcode_folders) != 3:
 		raise Exception("Expected 3 barcode folders! Check patient folder {pf}.".format(pf=pf))
 	try:
@@ -42,47 +88,6 @@ for pf in patient_folders:
 		raise n
 	#now barcode_folders is the same, minus the presence of the control barcode
 	for bf in barcode_folders:
-		run_gene_ebseq(treatment_folder=bf,control_folder=control_barcode_folder,patient_id=patient_id)
-		run_isoform_ebseq(treatent_folder=bf,control_folder=control_barcode_folder,patient_id=patient_id)
+#		run_gene_ebseq(treatment_folder=bf,control_folder=control_barcode_folder,patient_id=patient_id)
+		run_isoform_ebseq(treatment_folder=bf,control_folder=control_barcode_folder,patient_id=patient_id)
 
-
-def run_gene_ebseq(treatment_folder,control_folder,patient_id):
-	"""
-	Runs the 'rsem-ebseq gene expression' workflow I wrote.
-	"""
-	gene_glob = "*.genes.results"
-	quant_file = dxpy.find_data_object(more_ok=False,project=jo_wu_project,folder=technical_rep_folder,name=gene_glob,name_mode="glob")
-	control_quant_file = dxpy.find_data_object(more_ok=False,project=jo_wu_project,folder=control_folder,name=gene_glob,name_mode="glob")
-	#the quant_files are the isoforms gene expression files created by rsem-calculate-expression
-	treatment_barcode = os.path.split(treatment_folder)[-1]
-	control_barcode = os.path.split(control_folder)[-1]
-	wf = dxpy.DXWorkflow(project="project-BxxYqbQ0v3VQz5z2bvFyF6YV",dxid="workflow-ByBvBkj0v3VkyPVZfVvxg2Q3")
-	destination_folder = os.path.join(treatment_folder,"ebseq")
-	workflow_input = {"0.results":[quant_file,control_quant_file],"1.conditions": "1,1"}
-	job_properties = {"patient_id":patient_id,"control_barcode":control_barcode,"treatment_barcode":treatment_barcode}
-	job_name = "_".join([patient_id,treatment_barcode,control_barcode,wf.name])
-	wf.run(debug={"debugOn":["AppError","AppInternalError"],workflow_input=workflow_input,project=joseph_wu_project,folder=destination_folder,name=job_name,properties=job_properties)
-	
-
-def run_isoform_ebseq(treatment_folder,control_folder,patient_id):
-	"""
-	Runs the 'rsem-ebseq isoform expression' workflow I wrote.
-	"""
-	isoform_glob = "*.isoforms.results"
-	quant_file = dxpy.find_one_data_object(more_ok=False,project=jo_wu_project,folder=treatment_folder,name=isoform_glob,name_mode="glob")
-	control_quant_file = dxpy.find_data_object(more_ok=False,project=jo_wu_project,folder=control_folder,name=isoform_glob,name_mode="glob")
-	#the quant_files are the isoforms gene expression files created by rsem-calculate-expression
-	treatment_barcode = os.path.split(treatment_folder)[-1]
-	control_barcode = os.path.split(control_folder)[-1]
-	wf = dxpy.DXWorkflow(project="project-BxxYqbQ0v3VQz5z2bvFyF6YV",dxid="workflow-ByBq29Q0v3VfxzgV043QZ57J")
-	destination_folder = os.path.join(treatment_folder,"ebseq")
-	transcripts_file = dxpy.DXFile(project-BxZpbXj0V610b5Q6x1FV80gb:file-ByBpjPQ0V61585gqXkvQ11Z5)
-	workflow_input = {"0.results":[quant_file,control_quant_file],"1.input_fasta_file":dxpy.dxlink(transcripts_file),"1.output_name":"isoforms.results","2.conditions": "1,1"}
-	job_properties = {"patient_id":patient_id,"control_barcode":control_barcode,"treatment_barcode":treatment_barcode}
-	job_name = "_".join([patient_id,treatment_barcode,control_barcode,wf.name])
-	wf.run(debug={"debugOn":["AppError","AppInternalError"],workflow_input=workflow_input,project=joseph_wu_project,folder=destination_folder,name=job_name,properties=job_properties)
-	
-	
-    #ebseq_folder = os.path.join(barcode_folder,"ebseq")
-    #p.new_folder(ebseq_folder)
-				
